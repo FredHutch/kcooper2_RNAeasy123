a0995fea47247cc8c3824c40ca1b8220fe4de052
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-08-05 14:01:01 -0700

    changes from amy

--- app.R
+++ app.R
@@ -13,21 +13,15 @@ library(RColorBrewer)
 library(Glimma)
 library(gplots)
 library(msigdbr)
+library(DT)
 
-
-getCounts <- function() {
-  counts <- read_delim("2019.07.12.counts.txt", 
-                     "\t", escape_double = FALSE, trim_ws = TRUE)
-  # # rownames(counts) <- counts$GeneSymbol
-
-}
 
 getFileChoices <- function() {
   files <- list.files()
   files[grep(".txt", files, fixed=TRUE)]
 }
 
-
+filesAvailable <- getFileChoices() # Get file list to populate the drop down menu
 
 ui <- fluidPage(
 
@@ -40,18 +34,21 @@ ui <- fluidPage(
     # Sidebar panel for inputs ----
     sidebarPanel(
         selectInput("fileChooser", "Choose a file",
-          getFileChoices()),
+          filesAvailable),
         # for now we are hardcoding the cell types
         # TODO fix that....
         selectInput("cellTypeChooser", "Choose a cell type",
-          c("DC", "DP"))
+          c("DC", "DP")
+        ),
+        actionButton(inputId = "submitButton",
+          label = "Submit")
 
     ),
 
     # Main panel for displaying outputs ----
     mainPanel(
       tabsetPanel(type="tabs",
-        tabPanel("Counts", dataTableOutput("mytable")),
+        tabPanel("Counts", DTOutput("mytable")),
         tabPanel("Unsup. Clustering",
           fluidRow(
             column(6, plotOutput("leftUSPlot")),
@@ -63,65 +60,59 @@ ui <- fluidPage(
   )
 )
 
-makeUSPlot <- function(input) {
-    counts <- read_delim(input$fileChooser, 
-        "\t", escape_double = FALSE, trim_ws = TRUE)
-    rownames(counts) <- counts$GeneSymbol
-
-    cells <- dplyr::select(counts, contains(input$cellTypeChooser))
-    counts.m <- as.matrix(cells)
-    data <- DGEList(counts.m)
-    samplenames <- substring(colnames(data), 1, nchar(colnames(data)))
-    colnames(data) <- samplenames
-    # batch is hardcoded atm - TODO FIXME
-    batch <- as.factor(c("09182018", "09182018", 
-                     "09182018", "09182018", "09182018", 
-                     "10312018", "10312018"))
-    data$samples$batch <- batch
-    group <- as.factor(rep(c("D0", "D4", "D7"), c(2,3,2)))
-    data$samples$group <- group
-    geneid <- rownames(cells)
-    genes <- select(Mus.musculus, keys=geneid, columns=c("ENTREZID", "TXCHROM"), 
-                    keytype="SYMBOL")
-    genes <- genes[!duplicated(genes$SYMBOL),]
-    data$genes <- genes
-    cpm <- cpm(data)
-    lcpm <- cpm(data, log=TRUE)
-    L <- mean(data$samples$lib.size) * 1e-6
-    M <- median(data$samples$lib.size) * 1e-6
-    keep <- filterByExpr(data)
-    edgeR.data <- data[keep, , keep.lib.sizes=FALSE]
-    keep.exprs <- filterByExpr(data, group=group)
-    data <- data[keep.exprs,, keep.lib.sizes=FALSE]
-    data <- calcNormFactors(data, method = "TMM")
-    cpm <- cpm(data)
-    lcpm <- cpm(data, log=TRUE)
-    par(mfrow=c(1,2))
-    col.group <- group
-    levels(col.group) <-  brewer.pal(nlevels(col.group), "Set1")
-    col.group <- as.character(col.group)
-    col.batch <- batch
-    levels(col.batch) <-  brewer.pal(nlevels(col.batch), "Set2")
-    col.batch <- as.character(col.batch)
-    return (list(lcpm=lcpm, group=group, batch=batch, col.group=col.group, col.batch=col.batch, dim=c(3,4)))
-}
+# add an action button on the left side
 
 
 
 server <- function(input, output) {
-  output$mytable = shiny::renderDataTable({
-      read_delim(input$fileChooser, 
-        "\t", escape_double = FALSE, trim_ws = TRUE)
-  })
+  # TODO add a function to get the data
 
+  getData <- eventReactive(input$submitButton, {
+      counts <- data.frame(read_delim(input$fileChooser,
+                           "\t", escape_double = FALSE, trim_ws = TRUE))
+      theseCells <- dplyr::select(counts, contains(input$cellTypeChooser))
+      rownames(theseCells) <- counts$GeneSymbol
+      theseCells
+    })
+  getMetaData <- eventReactive(input$submitButton, {
+    metadataFile <- gsub("\\.txt$", ".csv", input$fileChooser)
+    metadataFile <- gsub("\\.counts\\.", ".metadata.", metadataFile)
+    metadata <- data.frame(read.csv(metadataFile), stringsAsFactors = TRUE)
+    thisMeta <- dplyr::filter(metadata, grepl(input$cellTypeChooser, sampleName)==TRUE)
+    thisMeta
+  })
+  
+  output$mytable = DT::renderDT({
+      getData()
+  }, rownames = TRUE, colnames = c('Gene Symbol' = 1))
+
+  makeUSPlot <- eventReactive(input$cellTypeChooser, {
+    cells <- getData()
+    metadata <- getMetaData()
+    counts.m <- as.matrix(cells)
+    data <- DGEList(counts.m)
+    colnames(data) <- metadata$sampleName
+    data$samples$batch <- metadata$batch
+    data$samples$group <- metadata$group
+    data$genes <- select(Mus.musculus, keys=rownames(cells), 
+                              columns=c("ENTREZID", "TXCHROM"), 
+                              keytype="SYMBOL") %>% filter(!duplicated(SYMBOL))
+    lcpm <- cpm(data, log=TRUE)
+    # col.batch <- batch
+    col.batch <- metadata$batch
+    levels(col.batch) <-  brewer.pal(nlevels(metadata$batch), "Set2")
+    col.batch <- as.character(col.batch)
+    return (list(lcpm=lcpm, batch=metadata$batch, col.batch=col.batch, dim=c(3,4)))
+  })
+  
 
   output$leftUSPlot <- renderPlot({
-    left <- makeUSPlot(input)
+    left <- makeUSPlot()
     plotMDS(left$lcpm, labels=left$group, col=left$col.group)
   })
 
   output$rightUSPlot <- renderPlot({
-    right <- makeUSPlot(input)
+    right <- makeUSPlot()
     plotMDS(right$lcpm, labels=right$batch, col=right$col.batch, dim=right$dim)
   })
 
