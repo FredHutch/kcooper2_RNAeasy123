3bb7bff86a7f1cc50aa7834c913fe0eccdd4c6ae
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-08-06 11:11:58 -0700

    revert accidental truncation

--- rnaseq123.DC.07152019.R
+++ rnaseq123.DC.07152019.R
@@ -149,3 +149,98 @@ de.common <- which(de[,1]!=0 & de[,2]!=0)
 length(de.common)
 #[1] 566
 head(efit$genes$SYMBOL[de.common], n=20)
+
+#Generate venn diagrams for shared up-regulated and down-regulated genes
+#For Shiny- generate venn diagrams for user, choice in color?
+vennDiagram(de[,1:3], include = "up",
+            circle.col=c("red", "blue", "green"), main= "Up-regulated genes in dendritic cells, eBays")
+vennDiagram(dt[,1:3], include = "up",
+            circle.col=c("red", "blue", "green"), main= "Up-regulated genes in dendritic cells, LFC")
+
+vennDiagram(de[,1:3], include = "down",
+            circle.col=c("red", "blue", "green"), main= "Down-regulated genes in dendritic cells, eBays")
+vennDiagram(dt[,1:3], include = "down",
+            circle.col=c("red", "blue", "green"), main= "Down-regulated genes in dendritic cells, LFC")
+
+write.fit(tfit, dt, file="DEresults.lfc.txt")
+
+#Examining individual DE genes from top to bottom
+DC.d0vsd4 <- topTreat(tfit, coef=1, n=Inf)
+DC.d0vsd7 <- topTreat(tfit, coef=2, n=Inf)
+DC.d4vsd7 <- topTreat(tfit, coef=3, n=Inf)
+head(DC.d0vsd4)
+
+#MD plots of differential expression results
+#For Shiny- generate MD plots
+plotMD(efit, column=1, status=de[,1], main=colnames(efit)[1], 
+       xlim=c(-5,15))
+plotMD(tfit, column=1, status=dt[,1], main=colnames(tfit)[1], col= c("blue", "red"),
+       xlim=c(-5,15))
+plotMD(tfit, column=2, status=dt[,2], main=colnames(tfit)[2], col= c("blue", "red"),
+       xlim=c(-5,15))
+plotMD(tfit, column=3, status=dt[,3], main=colnames(tfit)[3], col= c("blue", "red"),
+       xlim=c(-5,15))
+
+#Glimma MD plots
+#For Shiny- can Glimma be integrated? Does CSS play well with Shiny?
+library(Glimma)
+glMDPlot(tfit, coef=3, status=dt, main=colnames(tfit)[3],
+         side.main="SYMBOL", counts=lcpm, groups=group, launch=FALSE)
+
+#Generate heatmap
+#For Shiny- generate heatmap for user, allow user to determine how many genes plotted, what comparison to plot
+library(gplots)
+DC.d0vsd4.topgenes <- DC.d0vsd4$SYMBOL[1:100]
+i <- which(v$genes$SYMBOL %in% DC.d0vsd4.topgenes)
+mycol <- colorpanel(1000,"blue","white","red")
+heatmap.2(lcpm[i,], scale="row",
+          labRow=v$genes$SYMBOL[i], labCol=rownames(v$targets), 
+          col=mycol, trace="none", density.info="none", 
+          margin=c(7,5), lhei=c(2,10), dendrogram="column",
+          keysize = 1)
+
+DC.d0vsd7.topgenes <- DC.d0vsd7$SYMBOL[1:100]
+d7 <- which(v$genes$SYMBOL %in% DC.d0vsd7.topgenes)
+#mycol <- colorpanel(1000,"blue","white","red")
+heatmap.2(lcpm[d7,], scale="row",
+          labRow=v$genes$SYMBOL[d7], labCol=rownames(v$targets), 
+          col=mycol, trace="none", density.info="none", 
+          margin=c(7,6), lhei=c(2,10), dendrogram="column",
+          keysize = 1)
+
+DC.d4vsd7.topgenes <- DC.d4vsd7$SYMBOL[1:100]
+both <- which(v$genes$SYMBOL %in% DC.d4vsd7.topgenes)
+#mycol <- colorpanel(1000,"blue","white","red")
+heatmap.2(lcpm[both,], scale="row",
+          labRow=v$genes$SYMBOL[both], labCol=rownames(v$targets), 
+          col=mycol, trace="none", density.info="none", 
+          margin=c(7,6), lhei=c(2,10), dendrogram="column",
+          keysize = 1)
+
+#Gene set testing with camera
+#install.packages("msigdbr")
+library(msigdbr)
+#For Shiny- user selects gene set collection, view results of head() and choose direction
+#Retrieve mouse curated collection (C2) gene sets
+load("MSigDB/mouse_c2_v5p2.rdata")
+idx <- ids2indices(Mm.c2,id=v$genes$ENTREZID)
+cam.DC.d0vsd4 <- camera(v,idx,design,contrast=contr.matrix[,1])
+head(cam.DC.d0vsd4,5)
+head(cam.DC.d0vsd4[cam.DC.d0vsd4$Direction == "Up", ], 5)
+write.csv(cam.DC.d0vsd4, "camera.DC.d0vsd4.csv")
+
+cam.DC.d0vsd7 <- camera(v,idx,design,contrast=contr.matrix[,2])
+head(cam.DC.d0vsd7,10)
+write.csv(cam.DC.d0vsd7, "camera.DC.d0vsd7.csv")
+
+cam.DC.d4vsd7 <- camera(v,idx,design,contrast=contr.matrix[,3])
+head(cam.DC.d4vsd7,5)
+write.csv(cam.DC.d4vsd7, "camera.DC.d4vsd7.csv")
+
+#Barcode example
+#For Shiny- generate barcode, choose gene set
+dev.off()
+barcodeplot(tfit$t[,1], index=idx$NEMETH_INFLAMMATORY_RESPONSE_LPS_UP, 
+            index2=idx$REACTOME_GENERIC_TRANSCRIPTION_PATHWAY, main="DC D0vsD4")
+
+sessionInfo()
